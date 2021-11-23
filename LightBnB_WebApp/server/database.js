1a960f4800f0025aa68fe077958834eed2d6c8ff
Miguel <mige.d@outlook.com>
2021-11-23 17:15:12 -0500

    connect reservations function to db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -79,7 +79,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+    SELECT *
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    WHERE guest_id = $1
+    LIMIT $2
+  `;
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+    .then((res) => res.rows)
+    .catch((err) => {
+      console.log('query error:', err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
