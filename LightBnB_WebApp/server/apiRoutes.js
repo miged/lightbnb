5f020139a19bae8af7630e69913dd9674a0f18e9
Miguel <mige.d@outlook.com>
2021-11-23 15:03:28 -0500

    fix linting errors

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -2,38 +2,38 @@ module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
-    .then(properties => res.send({properties}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    }); 
+      .then((properties) => res.send({properties}))
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.get('/reservations', (req, res) => {
-    const userId = req.session.userId;
+    const {userId} = req.session;
     if (!userId) {
       res.error("💩");
       return;
     }
     database.getAllReservations(userId)
-    .then(reservations => res.send({reservations}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    });
+      .then((reservations) => res.send({reservations}))
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.post('/properties', (req, res) => {
-    const userId = req.session.userId;
+    const {userId} = req.session;
     database.addProperty({...req.body, owner_id: userId})
-      .then(property => {
+      .then((property) => {
         res.send(property);
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(e);
-        res.send(e)
+        res.send(e);
       });
   });
 
   return router;
-}
+};
--- LightBnB_WebApp/server/userRoutes.js
+++ LightBnB_WebApp/server/userRoutes.js
@@ -7,15 +7,15 @@ module.exports = function(router, database) {
     const user = req.body;
     user.password = bcrypt.hashSync(user.password, 12);
     database.addUser(user)
-    .then(user => {
-      if (!user) {
-        res.send({error: "error"});
-        return;
-      }
-      req.session.userId = user.id;
-      res.send("🤗");
-    })
-    .catch(e => res.send(e));
+      .then((user) => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send("🤗");
+      })
+      .catch((e) => res.send(e));
   });
 
   /**
@@ -25,19 +25,19 @@ module.exports = function(router, database) {
    */
   const login =  function(email, password) {
     return database.getUserWithEmail(email)
-    .then(user => {
-      if (bcrypt.compareSync(password, user.password)) {
-        return user;
-      }
-      return null;
-    });
-  }
+      .then((user) => {
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
+      });
+  };
   exports.login = login;
 
   router.post('/login', (req, res) => {
     const {email, password} = req.body;
     login(email, password)
-      .then(user => {
+      .then((user) => {
         if (!user) {
           res.send({error: "error"});
           return;
@@ -45,7 +45,7 @@ module.exports = function(router, database) {
         req.session.userId = user.id;
         res.send({user: {name: user.name, email: user.email, id: user.id}});
       })
-      .catch(e => res.send(e));
+      .catch((e) => res.send(e));
   });
   
   router.post('/logout', (req, res) => {
@@ -54,14 +54,14 @@ module.exports = function(router, database) {
   });
 
   router.get("/me", (req, res) => {
-    const userId = req.session.userId;
+    const {userId} = req.session;
     if (!userId) {
       res.send({message: "not logged in"});
       return;
     }
 
     database.getUserWithId(userId)
-      .then(user => {
+      .then((user) => {
         if (!user) {
           res.send({error: "no user with that id"});
           return;
@@ -69,8 +69,8 @@ module.exports = function(router, database) {
     
         res.send({user: {name: user.name, email: user.email, id: userId}});
       })
-      .catch(e => res.send(e));
+      .catch((e) => res.send(e));
   });
 
   return router;
-}
+};
